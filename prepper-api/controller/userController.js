29fe0cb66500863d0565caef6e687148088f8782
zzzuh <zejunlin790@gmail.com>
2024-01-24 11:59:04 -0500

    implemented logout function

--- prepper-api/controller/userController.js
+++ prepper-api/controller/userController.js
@@ -4,11 +4,72 @@ const jwt = require("jsonwebtoken");
 const pool = require("../config/database");
 
 const signup = async (req, res) => {
+    try {
+        const data = req.body;
+
+        if (!data.username || !data.password) {
+            res.status(404).send("Fields cannot be empty");
+            return;
+        }
+
+        const user = await pool.query("SELECT * FROM user WHERE username = $1", [data.username]);
+
+        if (user.row.length !== 0) {
+            res.status(404).send("Username already exists");
+            return;
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(data.password, salt);
+
+        const registeredUser = await pool.query("INSERT INTO user (username, password, picture) VALUES($1, $2, $3) RETURNING *", [data.username, hashPassword, data.picture]);
+        res.json(registeredUser.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+    }
 };
 
 const signin = async (req, res) => {
+    try {
+        const data = req.body;
+        if (!data.username || !data.password) {
+            res.status(404).send("Fields cannot be empty");
+            return;
+        }
+
+        const user = await pool.query("SELECT * FROM user WHERE username = $1", [data.username]);
+
+        if (user.rows.length === 0) {
+            res.status(404).send("Invalid username or password");
+            return;
+        }
+
+        const isValidPassword = await bcrypt.compare(data.password, user.rows[0].password);
+
+        if (!isValidPassword) {
+            res.status(404).send("Invalid username or password");
+        } else {
+            const token = jwt.sign(user.rows[0], process.env.JWT_SECRET);
+            res.cookie('token', token, {httpOnly: true});
+            res.status(200).send("Logged in");
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+    }
 
 };
+
+const logout = async (req, res) => {
+    try {
+        res.clearCookie('token');
+        res.status(200).send("Logged out success");
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+    }
+}
 
 const getUser = async (req, res) => {
     try {
@@ -82,7 +143,7 @@ const deleteUser = async (req, res) => {
             return;
         }
 
-        res.status(200).send(`User ${id} deleted`);
+        res.json(deleteUser.rows[0]);
     } catch (error) {
         console.error(error);
         res.status(500)
